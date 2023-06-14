d6ee74e0e60b393bf34472060a1d457e84e906dd
Fabian Schramm <55981657+fabinsch@users.noreply.github.com>
2023-06-14 17:14:12 +0200

    solo diffsim use utils

--- examples/solo_diffsim_jump.py
+++ examples/solo_diffsim_jump.py
@@ -11,72 +11,17 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 import proxddp
+import meshcat
 
 from proxnlp import constraints
 from proxddp import manifolds
 from proxddp.dynamics import ExplicitDynamicsModel, ExplicitDynamicsData
 from utils import ArgsBase
 
-from diffsim.simulator import Simulator, SimulatorNode
-from diffsim.shapes import Plane, Ellipsoid
-from diffsim.collision_pairs import CollisionPairPlaneEllipsoid, CollisionPairEllipsoidEllipsoid
 from diffsim.utils_render import init_viewer_ellipsoids
+from diffsim_rs_utils import create_solo_model, DiffSimDyamicsModel
+from diffsim.simulator import Simulator, SimulatorNode
 
-from pinocchio.visualize import MeshcatVisualizer
-
-def create_solo_model():
-   # robot model
-    robot = erd.load('solo12')
-    rmodel = robot.model.copy()
-    rdata = rmodel.createData()
-    rmodel.qref = rmodel.referenceConfigurations["standing"]
-    rmodel.qinit = rmodel.referenceConfigurations["standing"]
-    # Geometry model
-    rgeomModel = robot.collision_model
-    # add feet
-    a = 0.01910275 #1730829468
-    rFoot = np.array([0.2, 0.08, 0.05])
-    R = np.eye(3)
-    tLeft = np.array([0, 0.008,  -0.16])
-    MLeft = pin.SE3(R, tLeft)
-    tRight = np.array([0, -0.008,  -0.16])
-    MRight = pin.SE3(R, tRight)
-    r = np.array([a, a, a])  
-    
-    rgeomModel.computeColPairDist = []
-
-    n = np.array([0., 0., 1])
-    p = np.array([0., 0., 0.0])
-    h = np.array([100., 100., 0.01])
-    plane_shape = Plane(0, 'plane', n, p, h)
-    T = pin.SE3(plane_shape.R, plane_shape.t)
-    plane = pin.GeometryObject("plane", 0, 0, plane_shape, T)
-    plane.meshColor = np.array([0.5, 0.5, 0.5, 1.]) 
-    planeId = rgeomModel.addGeometryObject(plane)
-    
-    frames_names = ["HR_FOOT","HL_FOOT","FR_FOOT","FL_FOOT"]
-    
-    
-    rgeomModel.collision_pairs = []
-    for name in frames_names:
-        frame_id = rmodel.getFrameId(name)
-        frame = rmodel.frames[frame_id]
-        joint_id = frame.parent
-        frame_placement = frame.placement
-        
-        shape_name = name + "_shape"
-        shape = Ellipsoid(joint_id, shape_name , r, frame_placement)
-        geometry = pin.GeometryObject(shape_name, joint_id, shape, frame_placement)
-        geometry.meshColor = np.array([1.0, 0.2, 0.2, 1.])
-        
-        geom_id = rgeomModel.addGeometryObject(geometry)
-        
-        foot_plane = CollisionPairPlaneEllipsoid(planeId, geom_id)
-        rgeomModel.collision_pairs += [foot_plane]
-        rgeomModel.computeColPairDist.append(False)
-    rgeom_data = rgeomModel.createData()
-    ddl = np.array([i for i in range(6,rmodel.nv)])
-    return rmodel, rgeomModel, robot.visual_model, rdata, rgeom_data, ddl
 
 rmodel, rgeom_model, rvisual_model, rdata, rgeom_data, _ = create_solo_model()
 nq = rmodel.nq
@@ -107,55 +52,7 @@ class Args(ArgsBase):
         if self.record:
             self.display = True
 
-class DiffSimDyamicsModel(ExplicitDynamicsModel):
-
-    def __init__(self, space, model, actuation, geom_model, coeff_friction, coeff_rest, nu: int, dt: float, N_samples : int = 1, noise_intensity: float = 0.) -> None:
-        super().__init__(space, nu)
-        self.act = actuation
-        self.dt = dt
-        self.sim = SimulatorNode(Simulator(model, geom_model, dt, coeff_friction, coeff_rest, dt_collision=dt, eps_contact=1e-3))
-        self.N_samples = N_samples
-        self.noise_intensity = noise_intensity
-
-    def forward(self, x, u, data):
-        data.x_tens_ = torch.tensor(x, requires_grad=True)
-        data.u_tens_ = torch.tensor(u, requires_grad=True)
-        act_tens = torch.from_numpy(self.act)
-        data.xnext_tens_ = torch.zeros_like(data.x_tens_)
-        for i in range(self.N_samples):
-            u_noisy = data.u_tens_+ data.u_noise[i]
-            # print(f"data.x_tens_.shape {data.x_tens_.shape}")
-            # print(f"u_noisy.shape {u_noisy.shape}")
-            data.xnext_tens_ += self.sim.makeStep(data.x_tens_, act_tens @ u_noisy, calcPinDiff=True)
-        data.xnext_tens_ /= self.N_samples
-        data.xnext[:] = data.xnext_tens_.detach().numpy()
-        return
-    
-    def dForward(self, x_, u_, data):
-        for i in range(self.space.ndx):
-            grads = torch.autograd.grad(data.xnext_tens_[i], [data.x_tens_,data.u_tens_],retain_graph=True)
-            data.Jx[i,:]  = grads[0].detach().numpy()
-            data.Ju[i,:] = grads[1].detach().numpy()
-        return
-    
-    def computeJacobians(self, x, u, y, data):
-        print("calling computeJacobians custom")
-        data.u_noise = self.noise_intensity*torch.randn((self.N_samples,)+data.u_tens_.size())
-        ExplicitDynamicsData.computeJacobians(x,u,y,data)
-
-    def createData(self) -> ExplicitDynamicsData:
-        data = ExplicitDynamicsData(self.space.ndx, self.nu, self.nx2, self.ndx2)
-        data.u_tens_ = torch.zeros(self.nu)
-        data.u_noise = torch.zeros((self.N_samples,)+data.u_tens_.size())
-        # shape_xnext = data.xnext.shape
-        # data.x_tens_ = torch.zeros(shape_xnext)
-        # data.xnext_tens_ = torch.zeros(shape_xnext)
-        return data
-
-
-
 def main(args: Args):
-    import meshcat
 
     os.makedirs("assets", exist_ok=True)
     print(args)
@@ -190,7 +87,7 @@ def main(args: Args):
     # dt = 0.01
     # Tf = 1.5
     dt = 5e-3
-    Tf = 0.5
+    Tf = 1.0
     nsteps = int(Tf / dt)
     print("nsteps: {:d}".format(nsteps))
 
@@ -208,12 +105,12 @@ def main(args: Args):
         B = torch.zeros((nv, nv))  # B is the actuation matrix when u is control including FF
         B[6:, 6:] = torch.eye(nv - 6)
         def compute_static_torque(node, x, u, S):
-            print(f"x: {x}, u: {u}")
+            # print(f"x: {x}, u: {u}")
             x_next = node.makeStep(torch.tensor(x), torch.tensor(S @ u), calcPinDiff=True)
             model = node.rmodel
             data = model.createData()
             q = node.rdata.q
-            print(f"q: {q}")
+            # print(f"q: {q}")
 
             u_static = pin.rnea(model, data, q, np.zeros(model.nv), np.zeros(model.nv))  # len(u_static) = 14
             Jn = node.rdata.Jn_
@@ -226,20 +123,17 @@ def main(args: Args):
             lamN_static = tau_lamN_lamT[model.nv:model.nv+4]
             lamT_static = tau_lamN_lamT[model.nv+4:]
 
-            print(f"tau_static: {tau_static}")
+            # print(f"tau_static: {tau_static}")
             # print(f"lamN_static: {lamN_static}")
             # print(f"lamT_static: {lamT_static}")
-
             return tau_static_[6:].detach().numpy(), x_next.detach().numpy()
 
         u_list, x_list = [], [x0]
         first_node = nodes[0]
-        print(f"first_node")
         u, _ = compute_static_torque(first_node, x0, u0, S)
 
         x = 1*x0
-        print("other nodes")
-        for node in nodes:
+        for i, node in enumerate(nodes):
             u, x = compute_static_torque(node, x, u, S)
             u_list.append(1*u)
             x_list.append(1*x)
@@ -256,7 +150,8 @@ def main(args: Args):
     # xs_init = [x0] * (nsteps + 1)
 
     x_tar1 = 1*x0
-    # x_tar1[2] += 0.1  # jump up
+    # x_tar1[2] -= 0.05  # go down
+    x_tar1[2] += 0.1  # jump up
     # add_objective_vis_models(x_tar1)
 
     u_max = rmodel.effortLimit[-6:]
@@ -318,9 +213,6 @@ def main(args: Args):
         return prob
 
     problem = setup()
-
-    vizer = init_viewer_ellipsoids(rmodel, rgeom_model,rvisual_model, open=True)
-    vizer.display(pin.integrate(rmodel,rmodel.qref,x0[:nv]))
 
     tol = 1e-3
     verbose = proxddp.VerboseLevel.VERBOSE
@@ -408,6 +300,9 @@ def main(args: Args):
         plt.show()
 
     if args.display:
+        vizer = init_viewer_ellipsoids(rmodel, rgeom_model,rvisual_model, open=True)
+        vizer.display(pin.integrate(rmodel,rmodel.qref,x0[:nv]))
+
         cam_dist = 2.0
         directions_ = [np.array([1.0, 1.0, 0.5])]
         directions_.append(np.array([1.0, -1.0, 0.8]))
@@ -435,6 +330,12 @@ def main(args: Args):
             return _callback
 
         input("[enter to play]")
+        while True:
+            vizer.play(qs_opt, dt)
+            a = input("press to continue, [q] to quit")
+            if a == "q":
+                break
+
         if args.record:
             ctx = vizer.create_video_ctx(vid_uri, fps=30)
         else:
